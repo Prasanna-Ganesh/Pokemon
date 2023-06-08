3f6e836db833ca3c31f9e2e98b2c11e860f24d91
Prasanna-Ganesh <prasannaganesh579@gmail.com>
2023-06-08 15:22:06 +0530

    migration

--- src/app/config.py
+++ src/app/config.py
@@ -1,6 +1,5 @@
 class Config(object):
     DEBUG = True
-    DEVELOPMENT = True
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@localhost/pokemon"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PAGE_LIMIT = 20
